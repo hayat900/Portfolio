c270d598492155e74abeb6ef46fce59b64a19b0f
hayat900 <dishadsouza2000@gmail.com>
2021-11-19 06:54:49 +0530

    added routes

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter,Route } from 'react-router-dom';
 import ReactDom from 'react-dom';
+import About from '../About/About';
+import Projects from '../Projects/Projects';
+import Skills from '../Skills/Skills';
+import Contacts from '../Contacts/Contacts';
+import Home from '../Home/Home';
+import NavBar from '../NavBar/NavBar';
+import GoHome from '../GoHome/GoHome';
 const SampleComponent=()=>{
     return(
         <div>I am the first one</div>
@@ -12,11 +19,13 @@ const App=()=>{
     return(
         <BrowserRouter>
         <div>
-            <Route path="/" component={SampleComponent}/>
-            <Route path="/about" component={SampleComponent}/>
-            <Route path="/projects" component={SampleComponent}/>
-            <Route path="/skills" component={SampleComponent}/>
-            <Route path="/contact" component={SampleComponent}/>
+            <NavBar />
+            <GoHome />
+            <Route path="/" exact component={Home}/>
+            <Route path="/about" exact component={About}/>
+            <Route path="/projects" exact component={Projects}/>
+            <Route path="/skills" exact component={Skills}/>
+            <Route path="/contact" exact component={Contacts}/>
         </div>
         </BrowserRouter>
     );
---
+++ src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Contacts.css';
+const Contacts=()=>{
+    return(
+        <div>I am the home page</div>
+    );
+};
+export default Contacts;
